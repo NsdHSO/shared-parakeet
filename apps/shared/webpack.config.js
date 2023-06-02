55682315c1114c4ea24bd7d428c95d3700ba4322
( Nechiforel David-Samuel ) NsdHSO <nechiforelsamuel@yahoo.com>
2023-06-02 20:08:31 +0300

    chore:🌻 added name

--- apps/shared/webpack.config.js
+++ apps/shared/webpack.config.js
@@ -1,13 +1,14 @@
 const {withModuleFederationPlugin, shareAll} = require("@angular-architects/module-federation/webpack");
 module.exports = withModuleFederationPlugin({
   library: { type: "module" },
+  name: 'sharedDrawer',
 
   filename: "remoteEntry.js",
   name: 'frameDrawer',
   remotes: [],
   exposes: {
-    './FrameComponent': 'apps/shared/src/app/frame/frame.component',
-    './DrawerComponent': 'apps/shared/src/app/frame/frame-drawer/frame-drawer.component',
+    './Frame': 'apps/shared/src/app/frame/frame.component',
+    './Drawer': 'apps/shared/src/app/frame/frame-drawer/frame-drawer.component',
   },
   shared: {
     ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
