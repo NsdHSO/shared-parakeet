e4b93433a0982f9851a27e6d9afdfb5f5b347f78
( Nechiforel David-Samuel ) NsdHSO <nechiforelsamuel@yahoo.com>
2023-06-02 15:52:24 +0300

    chore:🌻 added some config

--- apps/shared/module-federation.config.js
+++ apps/shared/module-federation.config.js
@@ -1,4 +1,3 @@
-const {shareAll} = require("@angular-architects/module-federation/webpack");
 module.exports = {
   filename: "remoteEntry.js",
   name: 'shared',
@@ -7,7 +6,5 @@ module.exports = {
     'frame': 'apps/shared/src/app/frame/frame.component.ts',
     'drawer': 'apps/shared/src/app/frame/frame-drawer/frame-drawer.component.ts',
   },
-  shared: {
-    ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
-  },
+  
 };
